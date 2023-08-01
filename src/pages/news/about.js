708997729ff847dbef3c58220b45441f9c7a2b63
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-08-01 23:41:16 +0600

    about page

--- src/pages/news/about.js
+++ src/pages/news/about.js
@@ -4,6 +4,9 @@ const About = () => {
   return (
     <div>
       <h1>This is about Page</h1>
+      <h1>This is about Page</h1>
+      <h1>This is about Page</h1>
+      <h1>This is about Page</h1>
     </div>
   );
 };
